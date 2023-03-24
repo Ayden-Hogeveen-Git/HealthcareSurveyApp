c3541c817aa560eef8f491a7801692abbc69a5f6
Ayden Hogeveen <a.hogeveen@share.epsb.ca>
2023-03-24 14:56:39 -0600

    Initial Commit

---
+++ HealthcareSurveyApp/HealthcareSurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthcareSurveyAppApp.swift
+//  HealthcareSurveyApp
+//
+//  Created by Ayden Hogeveen on 2023-03-24.
+//
+
+import SwiftUI
+
+@main
+struct HealthcareSurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
