2beedadd7e17456df312f0d77cb40e169e5dfd37
Ayden Hogeveen <a.hogeveen@share.epsb.ca>
2023-03-27 16:52:47 -0600

    Got a working survey app up and running, next is just simple layout updates and creating database to store answers

--- HealthcareSurveyApp/ContentView.swift
+++ HealthcareSurveyApp/ContentView.swift
@@ -4,8 +4,11 @@
 //
 //  Created by Ayden Hogeveen on 2023-03-24.
 //
-// TODO: fix questionNum overflow error
-// TODO: add picker options
+// TODO: design updates
+// TODO: case 11 -- general text feedback
+// TODO: back button
+// TODO: Database management
+//
 
 import SwiftUI
 import UIKit
@@ -13,56 +16,109 @@ import UIKit
 
 struct ContentView: View {
     let screenWidth = UIScreen.main.bounds.width
-//    let leftOffset = UIScreen.main.bounds.width / 10
+    let screenHeight = UIScreen.main.bounds.height
     
     // Question and answer arrays
-    var questionArr = ["Unit is effectively staffed", "Support staff available", "Patient LOAD on unit", "Appropriate acuity level", "Resources avaiable for the shift", "Break times were observed", "Scheduling was collaborative", "I am heard", "PTO vacation approval", "Safety concerns"]
+    var questionArr = ["Unit is effectively staffed", "Support staff available", "Patient LOAD on unit", "Appropriate acuity level", "Resources avaiable for the shift", "Break times were observed", "Scheduling was collaborative", "I am heard", "PTO vacation approval", "Safety concerns", "What recommendations do you have to improve patient care during your shift? Please provide specific details and examples."]
     
-    let multAns = ["Yes", "Most of the time", "Neutral", "Sometimes", "Never", "N/A"]
+    let multAns = ["N/a", "Yes", "Most of the time", "Neutral", "Sometimes", "Never"]
         
     // Button vars
-    @State private var questionNum: Int = 0
-    @State private var answerArr =  Array<String>(repeating:"", count: 10)
+    @State private var questionNum: Int = -1
+    @State private var answerArr = Array<String>(repeating:"", count: 11)
     
     var body: some View {
         VStack {
-            
             switch (self.questionNum) {
-            case 0:
+            case -1:
                 VStack {
+                    Text("Healthcare Survey App")
+                        .font(.largeTitle)
+                        .padding()
+                    
                     Button(action: {
-                        self.questionNum = 1
+                        self.questionNum = 0
                     }, label: {
-                        Text("Question 1")
+                        Text("Start Survey")
                     })
+                    .font(.title2)
+                    .offset(y: screenHeight / 3)
                     .padding()
                 }
                 .padding()
-            case 1...10:
+            case 0...9:
                 // Display question and answer fields for questions
                 VStack {
                     Text(questionArr[self.questionNum])
                         .font(.largeTitle)
                         .foregroundColor(Color.gray)
-                        .position(x: screenWidth / 4, y: 50)
+                        .position(x: screenWidth / 2.5, y: 50)
                         .padding()
-                    TextField("Enter your answer: ", text: $answerArr[self.questionNum])
+                    
+                    Picker("Please enter your answer: ", selection: $answerArr[self.questionNum]) {
+                        ForEach(multAns, id: \.self) { answer in
+                            Text(answer)
+                        }
+                    }
 
+//                    Button(action: {
+//                        self.questionNum -= 1
+//                    }, label: {
+//                        Text("Back")
+//                    })
+//                    .position(x: 10, y: screenHeight / 1.9)
+//                    .padding()
+                    
                     Button(action: {
                         self.questionNum += 1
-//                        answerArr[self.questionNum] = ""
+                    }, label: {
+                        Text("Submit")
+                    })
+//                    .position(x: 110, y: screenHeight / 1.9)
+                    .padding()
+                    
+                    Text("Your answer was \(answerArr[self.questionNum])")
+                    .padding()
+                }
+                .padding()
+            case 10:
+                // Display question and answer field for last question
+                VStack {
+                    Text(questionArr[self.questionNum])
+                        .font(.title)
+                        .foregroundColor(Color.gray)
+                        .position(x: screenWidth / 2, y: 100)
+                        .padding()
+                    
+                    TextField("Feedback...", text: $answerArr[self.questionNum])
+                        .textFieldStyle(.roundedBorder)
+                        .multilineTextAlignment(.leading)
+                        .offset(y: -screenHeight / 2.75)
+                        .padding()
+                        
+                    
+                    Button(action: {
+                        self.questionNum += 1
                     }, label: {
                         Text("Submit")
                     })
                     .padding()
-                    Text("Your answer was \(answerArr[self.questionNum])")
-                        .padding()
                 }
-                .padding()
-            
             case 11:
                 VStack {
-                    Text("/(answerArr[0...9])")
+                    ForEach(answerArr, id: \.self) { answer in
+                        Text(answer)
+                    }
+                    
+                    // Save and Log answers to Database (database not yet implemented)
+                    Button(action: {
+                        self.questionNum = -1
+                        answerArr = Array<String>(repeating:"", count: 11)
+                    }, label: {
+                        Text("Save Answers")
+                    })
+                    
+                    
                 }
                 .padding()
             default:
@@ -73,8 +129,6 @@ struct ContentView: View {
                 .padding()
             }
         }
-        .padding()
-        
     }
 }
 
